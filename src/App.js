455e622a05691233860fa90198aadc6da7b42a44
Guilherme Raposo <gui.raposo36@gmail.com>
2022-02-16 22:20:34 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  const message = "Hello React";
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
