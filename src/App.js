184ab018cc3827d5f413f128bdcf4e730bce166f
Guilherme Raposo <gui.raposo36@gmail.com>
2022-02-16 23:50:32 -0300

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
+import People from "./assets/people.svg";
 
 function App() {
-  const message = "Hello React";
+  // ESTADO => VARIÁVEL, SÓ QUE, ESSA VARIÁVEL VAI PERMITIR QUE EU ATUALIZE OS ITENS NA TELA
+
+  const [comment, setComment] = useState();
+  const [allComment, setAllComment] = useState([]);
+
+  function ButtonComment() {
+    const allPreviousComment = [...allComment, comment];
+
+    setAllComment(allPreviousComment);
+    document.getElementById("TextArea").value = "";
+  }
+
+  function Digit(event) {
+    setComment(event.target.value);
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+      ButtonComment();
+    }
+  });
 
   return (
-    <div>
-      <h1>{message}</h1>
+    <div className="Container">
+      <img src={People} alt="Imagem pessoas" />
+      <textarea
+        id="TextArea"
+        placeholder="Seu comentário aqui !"
+        onChange={Digit}
+      ></textarea>
+      <button onClick={ButtonComment}>Comentar</button>
+
+      <ul>
+        {allComment.map((comment) => (
+          <li key={comment}>{comment}</li>
+        ))}
+      </ul>
     </div>
   );
 }
